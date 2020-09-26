ac59c9b8ea47e998dc1bb9ed083e1b7cd22853ee
Martin Gallagher <martin@praegressus.com>
2020-09-26 15:05:15 +0100

    Cargo fmt to appease lint check

--- benches/default.rs
+++ benches/default.rs
@@ -1,12 +1,15 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use html5minify::*;
 
-const HTML: &str = "<html> \n<link href=\"test.css\">\n<h2   id=\"id_one\"    >Hello\n</h2>    \n<p>\nWorld</p>";
+const HTML: &str =
+    "<html> \n<link href=\"test.css\">\n<h2   id=\"id_one\"    >Hello\n</h2>    \n<p>\nWorld</p>";
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("minify", |b| b.iter(|| {
-      black_box(HTML).minify().expect("Failed to minify HTML");
-    }));
+    c.bench_function("minify", |b| {
+        b.iter(|| {
+            black_box(HTML).minify().expect("Failed to minify HTML");
+        })
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
